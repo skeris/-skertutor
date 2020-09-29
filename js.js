44759e624eecf03cd54c74cbe5255279726ab5e9
KrokodilKa <Nastyab015@yandex.ru>
2020-09-29 15:29:02 +0300

    перед изменением grid

---
+++ js.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementsByClassName('rarr')[0].onclick = function () {console.log(1)}
+//console.log(document.getElementsByClassName('rarr')[0].outerHTML)
+}, false);
