7d353b17a7c46010ee790c2a9f23524cda3f0609
KrokodilKa <Nastyab015@yandex.ru>
2020-10-15 17:41:12 +0300

    Итог вёрстки

--- js.js
+++ js.js
@@ -1,4 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-document.getElementsByClassName('rarr')[0].onclick = function () {console.log(1)}
+//document.addEventListener('DOMContentLoaded', function() {
+//document.getElementsByClassName('rarr')[0].onclick = function () {console.log(1)}
 //console.log(document.getElementsByClassName('rarr')[0].outerHTML)
-}, false);
+//}, false);
+let rarr = document.getElementsByClassName('rarr')[0];
+let currentbg = 0;
+let bg0 = ["1.png", "2.png", "3.png",];
+rarr.onclick = () => {
+currentbg++
+block1.style.backgroundImage = 'url('+bg0[currentbg]+')'
+}//console.log (Math.floor(Math.random( ) * (10+1)));
---
+++ Сашина боёвка/js.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+ // Check for the various File API support.
+ if (window.File && window.FileReader && window.FileList && window.Blob) {
+    // Great success! All the File APIs are supported.
+  
+        function handleFileSelect(evt) {
+          evt.stopPropagation();
+          evt.preventDefault();
+      
+          var files = evt.dataTransfer.files; // FileList object.
+      
+          // files is a FileList of File objects. List some properties.
+        var output = [];
+          for (var i = 0, f; f = files[i]; i++) {
+            if (!f.type.match('text.*')) {
+              continue;
+            }
+          var reader = new FileReader();
+          reader.onload = (function(theFile) {
+          return function(e) {
+            if (e.explicitOriginalTarget.readyState = 2){
+            console.log(e.explicitOriginalTarget.result)
+            }
+          };
+        })(f);
+          reader.readAsText(f);
+          }
+          document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+        }
+      
+        function handleDragOver(evt) {
+          evt.stopPropagation();
+          evt.preventDefault();
+          evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+        }
+      
+        // Setup the dnd listeners.
+        var dropZone = document.getElementById('drop_zone');
+        dropZone.addEventListener('dragover', handleDragOver, false);
+        dropZone.addEventListener('drop', handleFileSelect, false);
+      } else {
+    alert('The File APIs are not fully supported in this browser.');
+      }
